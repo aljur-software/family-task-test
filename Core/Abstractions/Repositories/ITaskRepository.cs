2b4b60e713c1c23b69de23963935129cd052e033
JUK-ALJUR <krasnopolskaya.julia@aljur.com>
2020-10-24 22:01:43 +0300

    ITaskRepository was initually changed (need to be changed)

--- Core/Abstractions/Repositories/ITaskRepository.cs
+++ Core/Abstractions/Repositories/ITaskRepository.cs
@@ -1,10 +1,13 @@
-﻿using Domain.DataModels;
+﻿using ddm = Domain.DataModels;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Core.Abstractions.Repositories
 {
-    public interface ITaskRepository: IBaseRepository<Guid, Task, ITaskRepository>
+    public interface ITaskRepository: IBaseRepository<Guid, ddm.Task, ITaskRepository>
     {
-
+        Task<int> AssignTaskAsync(ddm.Task record, CancellationToken cancellationToken = default);
+        Task<int> CompleteTaskAsync(ddm.Task record, CancellationToken cancellationToken = default);
     }
 }
--- DataLayer/Repositories/TaskRepository.cs
+++ DataLayer/Repositories/TaskRepository.cs
@@ -1,22 +1,33 @@
 ﻿using Core.Abstractions.Repositories;
-using Domain.DataModels;
+using ddm = Domain.DataModels;
 using System;
+using System.Threading.Tasks;
+using System.Threading;
 
 namespace DataLayer.Repositories
 {
-    public class TaskRepository : BaseRepository<Guid, Task, TaskRepository>, ITaskRepository
+    public class TaskRepository : BaseRepository<Guid, ddm.Task, TaskRepository>, ITaskRepository
     {
         public TaskRepository(FamilyTaskContext context) : base(context)
         { }
 
-        ITaskRepository IBaseRepository<Guid, Task, ITaskRepository>.NoTrack()
+        ITaskRepository IBaseRepository<Guid, ddm.Task, ITaskRepository>.NoTrack()
         {
             return base.NoTrack();
         }
 
-        ITaskRepository IBaseRepository<Guid, Task, ITaskRepository>.Reset()
+        ITaskRepository IBaseRepository<Guid, ddm.Task, ITaskRepository>.Reset()
         {
             return base.Reset();
+        }
+
+        public async Task<int> AssignTaskAsync(ddm.Task record, CancellationToken cancellationToken = default)
+        {
+            return await base.UpdateRecordAsync(record, cancellationToken); // what if it was completed by other user?
+        }
+        public async Task<int> CompleteTaskAsync(ddm.Task record, CancellationToken cancellationToken = default)
+        {
+            return await base.UpdateRecordAsync(record, cancellationToken); // what if it was assigned by other user?
         }
     }
 }
