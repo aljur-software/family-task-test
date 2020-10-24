6d01d2caeb38b3754dca31f1421e8a50a3fd5c78
Aljur-Pavel-Dubina <pavel.dubina@aljur.com>
2020-10-24 18:08:01 +0300

    Implement ITaskService on TaskService. Add ITaskService to dependency injection.

---
+++ Services/TaskService.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Core.Abstractions.Repositories;
+using Core.Abstractions.Services;
+using Domain.Commands;
+using Domain.Queries;
+using Domain.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class TaskService : ITaskService
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly IMapper _mapper;
+
+        public TaskService(IMapper mapper, ITaskRepository taskRepository)
+        {
+            _mapper = mapper;
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<CreateTaskCommandResult> CreateTaskCommandHandler(CreateTaskCommand command)
+        {
+            var task = _mapper.Map<Domain.DataModels.Task>(command);
+            var persistedMember = await _taskRepository.CreateRecordAsync(task);
+
+            var vm = _mapper.Map<TaskVm>(persistedMember);
+
+            return new CreateTaskCommandResult()
+            {
+                Payload = vm
+            };
+        }
+
+        public Task<AssignTaskCommandResult> AssignTaskCommandHandler(AssignTaskCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CompleteTaskCommandResult> CompleteTaskCommandHandler(CompleteTaskCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetAllTasksQueryResult> GetAllTasksQueryHandler()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
